b27cbba6d87fc4fad09b756631d4d305261c7f2a
tande <tande1124@163.com>
2022-01-21 17:28:56 +0800

    vue源码

---
+++ src/core/instance/index.js
@@ -0,0 +1,19 @@
+import { initMixin } from "./init";
+import { stateMixin } from "./state";
+import { renderMixin } from "./render";
+import { eventsMixin } from "./events";
+import { lifecycleMixin } from "./lifecycle";
+
+// Vue构造函数
+function Vue(options) {
+  // Vue.protoype._init 方法, 该方法是在 initMixin中定义的
+  this._init(options);
+}
+
+initMixin(Vue);
+stateMixin(Vue);
+eventsMixin(Vue);
+lifecycleMixin(Vue);
+renderMixin(Vue);
+
+export default Vue;
---
+++ src/core/instance/init.js
@@ -0,0 +1,164 @@
+/* @flow */
+
+import config from "../config";
+import { initProxy } from "./proxy";
+import { initState } from "./state";
+import { initRender } from "./render";
+import { initEvents } from "./events";
+import { mark, measure } from "../util/perf";
+import { initLifecycle, callHook } from "./lifecycle";
+import { initProvide, initInjections } from "./inject";
+import { extend, mergeOptions, formatComponentName } from "../util/index";
+
+let uid = 0;
+
+/**
+ *  定义 Vue.prototype._init 方法
+ * @param {*} Vue 构造函数
+ */
+
+export function initMixin(Vue: Class<Component>) {
+  // 负责 Vue 的初始化过程
+  Vue.prototype._init = function (options?: Object) {
+    debugger;
+    // vue 实例
+    const vm: Component = this;
+
+    // 每个 Vue 实例都有一个 _uid,并且是依次递增的
+    vm._uid = uid++;
+
+    vm._isVue = true;
+    // 处理组件配置项
+    if (options && options._isComponent) {
+      // 子组件：性能优化 减少原型链的动态查找，提高执行效率
+      /**
+       * 每个子组件初始化时走这里，这里只做了一些性能优化
+       * 将组件配置对象上的一些深层次属性放到 vm.$options 选项中，以提高代码的执行效率
+       */
+      initInternalComponent(vm, options);
+    } else {
+      /**
+       * 初始化根组件走这里，合并vue 的全局配置到根组件的局部配置，比如 Vue.component 注册的全局组件会合并到根实例的 components选项中
+       * 至于每个子组件的选项合并则发生在两个地方
+       * 1、 Vue.components 方法注册的全局组件在注册时做了选项合并
+       * 2、{components: {xx}} 方法注册的局部组件在执行编译器生成的render 函数时做了选项合并，包括根组件中饭的components配置
+       */
+      vm.$options = mergeOptions(
+        resolveConstructorOptions(vm.constructor),
+        options || {},
+        vm
+      );
+    }
+
+    if (process.env.NODE_ENV !== "production") {
+      // 设置代理，将vm 实例上的属性代理到 vm._renderProxy
+      initProxy(vm);
+    } else {
+      vm._renderProxy = vm;
+    }
+    vm._self = vm;
+    // 初始化组件会理关系属性，比如：$parents、$children、$root、$refs等
+    initLifecycle(vm);
+    /**
+     * 初始化自定义事件，这里需要注意一点，所以在<comp @click="handClick"/> 上注册的事件，监听者不是父组件
+     * 而是子组件本身，也就是说事件的派发和监听者都是子组件本身，和父组件无法
+     */
+    initEvents(vm);
+    // 解析组件的插槽信息，得到vm.$slot,处理渲染函数，得到vm.$createElement方法，即h函数
+    initRender(vm);
+    // 调用 beforeCreate 钩子函数
+    callHook(vm, "beforeCreate");
+    // 初始化组件的inject配置项，得到result[key] = val 形式的配置对象，然后对结果数据进行响应式处理，并处理每个key到vm实例
+    initInjections(vm); // resolve injections before data/props
+    // 数据响应式的重点，处理props、methods、data、computed、watch
+    initState(vm);
+    // 解析组件配置项上的provide对象，将其挂载到vm._provided属性上
+    initProvide(vm); // resolve provide after data/props
+    // 调用 created钩子函数
+    callHook(vm, "created");
+
+    // 如果发现配置项上有 el 选项，则自动调用 $mount 方法，也就是说有了 el 选项，就不需要再手动调用 $mount，反之，没有 el 则必须手动调用 $mount
+    if (vm.$options.el) {
+      // 调用 $mount 方法，进入挂载阶段
+      vm.$mount(vm.$options.el);
+    }
+  };
+}
+
+// 性能优化，打平配置对象中上的属性，减少运行时原型链的查找，提高执行效率
+export function initInternalComponent(
+  vm: Component,
+  options: InternalComponentOptions
+) {
+  // 基于 构造函数 上的配置对象 创建vm.$options
+  const opts = (vm.$options = Object.create(vm.constructor.options));
+  // doing this because it's faster than dynamic enumeration.
+  const parentVnode = options._parentVnode;
+  opts.parent = options.parent;
+  opts._parentVnode = parentVnode;
+
+  const vnodeComponentOptions = parentVnode.componentOptions;
+  opts.propsData = vnodeComponentOptions.propsData;
+  opts._parentListeners = vnodeComponentOptions.listeners;
+  opts._renderChildren = vnodeComponentOptions.children;
+  opts._componentTag = vnodeComponentOptions.tag;
+
+  // 有 render 函数，将其赋值到vm.$options
+  if (options.render) {
+    opts.render = options.render;
+    opts.staticRenderFns = options.staticRenderFns;
+  }
+}
+
+/**
+ * 从组件构造函数中的解析配置对象options，并合并基类选项
+ * @param {*} Ctor
+ * @returns
+ */
+export function resolveConstructorOptions(Ctor: Class<Component>) {
+  // 配置项目
+  let options = Ctor.options;
+  if (Ctor.super) {
+    // 存于基类，递归解析基类构造函数的选项
+    const superOptions = resolveConstructorOptions(Ctor.super);
+    const cachedSuperOptions = Ctor.superOptions;
+    if (superOptions !== cachedSuperOptions) {
+      // 说明基类构造函数选项已经发生改变，需要重新设置
+      Ctor.superOptions = superOptions;
+      // 检查 Ctor.options 上是否有任何后期修改/附加的选项
+      const modifiedOptions = resolveModifiedOptions(Ctor);
+      // 如果存在被修改或增加的选项，则合并两个选项
+      if (modifiedOptions) {
+        extend(Ctor.extendOptions, modifiedOptions);
+      }
+
+      // 选项合并，将合并结果赋值为Ctor.options
+      options = Ctor.options = mergeOptions(superOptions, Ctor.extendOptions);
+      if (options.name) {
+        options.components[options.name] = Ctor;
+      }
+    }
+  }
+  return options;
+}
+
+/**
+ * 解析构造函数选项中后续被修改或者增加的选项
+ * @param {*} Ctor
+ * @returns
+ */
+function resolveModifiedOptions(Ctor: Class<Component>): ?Object {
+  let modified;
+  // 构造函数选项
+  const latest = Ctor.options;
+  // 密封的构造函数选项，备份
+  const sealed = Ctor.sealedOptions;
+  // 对比两个选项，记录不一样的选项
+  for (const key in latest) {
+    if (latest[key] !== sealed[key]) {
+      if (!modified) modified = {};
+      modified[key] = latest[key];
+    }
+  }
+  return modified;
+}
